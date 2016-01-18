fbc8e01f5d84068d2d41a04d97dd14ce2575df7b
모터그래프 개발01 <yj.kwak@motorgraph.com>
2016-01-18 18:03:56 +0900

    init

---
+++ hello.jsx
@@ -0,0 +1,21 @@
+/* CommonJS
+var React = require('react');
+
+var Hello = React.createClass({
+    displayName: 'Hello',
+    render: function () {
+        return React.createElement("h1", null, "Hello ");
+    }
+});
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('hello'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
+var config = {
+    entry: [
+        './src/App.js'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            }
+        ]
+    }
+}
+
+module.exports = config;
